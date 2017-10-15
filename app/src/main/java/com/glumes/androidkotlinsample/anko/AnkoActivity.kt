c249d93f6a875e46dbde8c83ed9d4ad8d8ae4f07
glumes <zhaoying9402@gmail.com>
2017-10-15 22:48:33 +0800

    init project

---
+++ app/src/main/java/com/glumes/androidkotlinsample/anko/AnkoActivity.kt
@@ -0,0 +1,13 @@
+package com.glumes.androidkotlinsample.anko
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.glumes.androidkotlinsample.R
+
+class AnkoActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_anko)
+    }
+}
